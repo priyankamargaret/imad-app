5f1c4672ccc3886c237926a603645d9f57701473
priyankamargaret <31609314+priyankamargaret@users.noreply.github.com>
2017-09-05 23:21:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,5 +8,5 @@ element.innerHTML = 'New Value';
 //Move
 var img = document.getElementById('madi');
 img.onclick = function() {
-    var interval = setInterval(moveleft,100);
+    var interval = setInterval(moveLeft,100);
 };
