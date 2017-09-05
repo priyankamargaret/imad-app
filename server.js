f8aba4121493575e1e7734bd9aabe65ae000ed96
priyankamargaret <31609314+priyankamargaret@users.noreply.github.com>
2017-09-05 23:49:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,6 +78,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articleName', function(req,res) {
     var articleName = req.params.articleName;
     res.send(creatTemplate(articles[articleName]));
